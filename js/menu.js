f44613eac9f48f3b656e81baf0172f7e93900437
Kostya Polishchuk <kostya.polishchuk@gmail.com>
2025-08-22 02:28:54 +0200

    add common styles to all pages

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,11 @@
 (() => {
   const refs = {
     // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
+    openModalBtn: document.querySelector("[data-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-mobile-menu]"),
+    modal: document.querySelector("[data-menu]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
